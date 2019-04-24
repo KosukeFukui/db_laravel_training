23dc92a4d5027145862a874934c07907342dbcd5
vagrant <vagrant@homestead>
2019-04-24 03:25:31 +0000

    コメント対応2019/04/24

--- Laravel/database/migrations/2019_04_22_095936_create_estimation_requests_table.php
+++ Laravel/database/migrations/2019_04_22_095936_create_estimation_requests_table.php
@@ -13,14 +13,17 @@ class CreateEstimationRequestsTable extends Migration
      */
     public function up()
     {
-        Schema::create('estimation_requests', function (Blueprint $table) {
-            $table->bigIncrements('estimation_request_id');
-	    $table->dateTime('estimation_request_date');
-	    $table->text('estimation_request_maker_name');
-	    $table->integer('partner_id')->unsigned();
-	    $table->text('partner_name');
-	    $table->text('partner_incharge_name');
-	    $table->date('desirable_answer_date');
+        Schema::table('estimation_requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('estimation_request_date');
+            $table->text('estimation_request_maker_name');
+            $table->integer('partner_id')->unsigned();
+            $table->text('partner_name');
+            $table->text('partner_incharge_name');
+            $table->date('desirable_answer_date');
+            $table->timestamps();
+            $table->text('create_user');
+            $table->text('update_user');
         });
     }
 
--- Laravel/database/migrations/2019_04_23_051701_create_estimation_request_details_table.php
+++ Laravel/database/migrations/2019_04_23_051701_create_estimation_request_details_table.php
@@ -13,15 +13,18 @@ class CreateEstimationRequestDetailsTable extends Migration
      */
     public function up()
     {
-        Schema::create('estimation_request_details', function (Blueprint $table) {
-            $table->bigIncrements('estimation_request_detail_id');
-	    $table->bigInteger('estimation_request_id')->unsigned();
-	    $table->mediumInteger('catalog_id')->unsigned();
-	    $table->text('catalog_name');
-	    $table->integer('product_id')->unsigned();
-	    $table->text('product_name');
-	    $table->integer('product_quantity')->unsigned();
-	    $table->foreign('estimation_request_id')->references('estimation_request_id')->on('estimation_requests');
+        Schema::table('estimation_request_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('estimation_request_id')->unsigned();
+            $table->mediumInteger('catalog_id')->unsigned();
+            $table->text('catalog_name');
+            $table->integer('product_id')->unsigned();
+            $table->text('product_name');
+            $table->integer('product_quantity')->unsigned();
+            $table->foreign('estimation_request_id')->references('id')->on('estimation_requests');
+            $table->timestamps();
+            $table->text('create_user');
+            $table->text('update_user');
         });
     }
 
