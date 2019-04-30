f7333d86032928b2772ed55f8f1ce4a91fa5d269
KosukeFukui <46667657+KosukeFukui@users.noreply.github.com>
2019-04-30 21:12:05 +0900

    Revert "レビュー依頼"

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-Route::get('training', 'TrainingController@index');
-Route::get('training/request', 'TrainingController@estimation_request');
-Route::get('training/request/{id}', 'TrainingController@estimation_request_detail');
-Route::get('training/request/{id}/new', 'TrainingController@estimation_request_detail_new');
-Route::post('training/request/{id}/new', 'TrainingController@estimation_request_detail_post');
+Route::get('/', function () {
+    return view('welcome');
+});
